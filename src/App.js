9ac568a32e9916044f0d8fb7cc00d91b7e027614
armancse-dev <armanhossaincse7@gmail.com>
2022-10-11 22:11:04 +0600

    Use Bootstrap From And Get User Email And Password

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ const providers = new GithubAuthProvider();
 
 function App() {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
   const [user, setUser] = useState({})
   const auth = getAuth();
   const handleGoogleSignIn = () =>{
@@ -55,23 +58,49 @@ function App() {
     })
   }
 
+  const handleEmailChange = e =>{
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e =>{
+    setPassword(e.target.value);
+  }
   const handleRegistration = e =>{
-    console.log('registration will be added');
+    console.log(email, password);
     e.preventDefault();
   }
 
   return (
-    <div className="App">
+    <div className="mx-5">
 
-      <form action="" onSubmit={handleRegistration}>
-        <h3>Please Register</h3>
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email" id="" /><br/>
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="" /><br/>
-        <input type="submit" value="Register" />
-      </form>
-      
+    <form onSubmit={handleRegistration}>
+      <h3 className='text-primary'>Pelase Register</h3>
+      <div class="row mb-3">
+        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input onBlur={handleEmailChange} type="email" class="form-control" id="inputEmail3"/>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+          <input onBlur={handlePasswordChange} type="password" class="form-control" id="inputPassword3"/>
+        </div>
+      </div>
+    
+      <div class="row mb-3">
+        <div class="col-sm-10 offset-sm-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck1"/>
+            <label class="form-check-label" for="gridCheck1">
+              Example checkbox
+            </label>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+          
       
       <br/><br/><br/><br/><br/>
       <div>-------------------------</div>
